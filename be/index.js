c616f2e0b07ad4a19d2b7b66e5747b12cfc32d84
raffichaesa <140777811+raffichs@users.noreply.github.com>
2025-03-26 21:47:12 +0700

    fix: port

--- be/index.js
+++ be/index.js
@@ -7,4 +7,4 @@ app.use(cors());
 app.use(express.json());
 app.use(NoteRoute);
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(8080, () => console.log("Server is running on port 8080"));
